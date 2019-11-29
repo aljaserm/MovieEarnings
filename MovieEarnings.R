4c58f27714b78401a4cd9212123d56a4ad53f8d0
Mohammed alJaser <aljaserm7@gmail.com>
2019-11-29 12:08:45 -0500

    to predict the movie erining

---
+++ MovieEarnings.R
@@ -0,0 +1,48 @@
+movies <- read.csv("C:/Users/aljas/OneDrive/Documents/Development/R/MovieEarnings/MovieEarnings/Movie_regression.csv", header = TRUE)
+# View(movies)
+
+summary(movies)
+movies$Time_taken[is.na(movies$Time_taken)] <- mean(movies$Time_taken, na.rm = TRUE)
+
+# Test-Train Split
+# install.packages('caTools')
+library(caTools)
+set.seed(0)
+split =sample.split(movies,SplitRatio = 0.8)
+train = subset(movies,split == TRUE)
+test = subset(movies,split == FALSE)
+
+#install required packages
+# install.packages('rpart')
+# install.packages('rpart.plot')
+library(rpart)
+library(rpart.plot)
+
+#Run regression tree model on train set
+regtree <- rpart(formula = Collection~., data = train, control = rpart.control(maxdepth = 3))
+#press F1 on rpart for help on this function
+
+#Plot the decision Tree
+rpart.plot(regtree, box.palette="RdBu", digits = -3)
+
+#Predict value at any point
+test$pred <- predict(regtree, test, type = "vector")
+
+MSE2 <- mean((test$pred - test$Collection)^2)
+
+#Tree Pruning
+fulltree <- rpart(formula = Collection~., data = train, control = rpart.control( cp = 0))
+rpart.plot(fulltree, box.palette="RdBu", digits = -3)
+printcp(fulltree)
+plotcp(regtree)
+
+mincp <- regtree$cptable[which.min(regtree$cptable[,"xerror"]),"CP"]
+
+prunedtree <- prune(fulltree, cp = mincp)
+rpart.plot(prunedtree, box.palette="RdBu", digits = -3)
+
+test$fulltree <- predict(fulltree, test, type = "vector")
+MSE2full <- mean((test$fulltree - test$Collection)^2)
+
+test$pruned <- predict(prunedtree, test, type = "vector")
+MSE2pruned <- mean((test$pruned - test$Collection)^2)
